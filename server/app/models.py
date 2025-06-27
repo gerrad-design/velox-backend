0e945bc92bc20979174729691a617cfc463ddcc4
gerrad <gerrad.meitamei@student.moringaschool.com>
2025-06-27 21:32:56 +0300

    added git ignore

--- server/app/models.py
+++ server/app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     phone = db.Column(db.String(20))
-    user_type = db.Column(db.String(10))  # 'rider' or 'driver'
+    user_type = db.Column(db.String(10))  
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
