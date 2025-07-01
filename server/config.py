0545c5ad39bd67e8984c12d546ac565c1191763a
Alvine Munene <alvine.munene@student.moringaschool.com>
2025-07-01 13:24:36 +0300

    Ft feedback (#8)
    * feedback
    
    * feedback backend
    
    * updated feedback backend

--- server/config.py
+++ server/config.py
@@ -7,8 +7,8 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret_key")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///feedback.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///velox.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "super-secret" 
+    JWT_SECRET_KEY = "super-secret"
